7a6df9a31b3a546befd66239ed206dafcc0d74b1
Johan RINALDI <johan.rinaldi@viacesi.fr>
2023-12-08 17:01:10 +0100

    form3

--- MyForm3.h
+++ MyForm3.h
@@ -82,6 +82,15 @@ namespace ProjetPOOGroupe2 {
 		System::ComponentModel::Container^ components;
 		AB::AccesBase^ acces_base;
 	private: System::Windows::Forms::Label^ label_stock;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::TextBox^ textBox6;
 		   StockageCommandes^ commandes;
 
 
@@ -109,6 +118,15 @@ namespace ProjetPOOGroupe2 {
 			   this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			   this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			   this->label_stock = (gcnew System::Windows::Forms::Label());
+			   this->button1 = (gcnew System::Windows::Forms::Button());
+			   this->label5 = (gcnew System::Windows::Forms::Label());
+			   this->label6 = (gcnew System::Windows::Forms::Label());
+			   this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			   this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			   this->label7 = (gcnew System::Windows::Forms::Label());
+			   this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			   this->label8 = (gcnew System::Windows::Forms::Label());
+			   this->textBox6 = (gcnew System::Windows::Forms::TextBox());
 			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			   this->SuspendLayout();
 			   // 
@@ -124,7 +142,7 @@ namespace ProjetPOOGroupe2 {
 			   this->dataGridView1->ReadOnly = true;
 			   this->dataGridView1->RowHeadersWidth = 51;
 			   this->dataGridView1->SelectionMode = System::Windows::Forms::DataGridViewSelectionMode::FullRowSelect;
-			   this->dataGridView1->Size = System::Drawing::Size(855, 196);
+			   this->dataGridView1->Size = System::Drawing::Size(429, 196);
 			   this->dataGridView1->TabIndex = 0;
 			   // 
 			   // btn_retour
@@ -148,7 +166,7 @@ namespace ProjetPOOGroupe2 {
 			   this->btn_ajouter->Name = L"btn_ajouter";
 			   this->btn_ajouter->Size = System::Drawing::Size(211, 67);
 			   this->btn_ajouter->TabIndex = 2;
-			   this->btn_ajouter->Text = L"AJOUTER";
+			   this->btn_ajouter->Text = L"AJOUTER COMMANDE";
 			   this->btn_ajouter->UseVisualStyleBackColor = false;
 			   this->btn_ajouter->Click += gcnew System::EventHandler(this, &MyForm3::btn_ajouter_Click);
 			   // 
@@ -180,7 +198,7 @@ namespace ProjetPOOGroupe2 {
 			   // 
 			   // tb_nom
 			   // 
-			   this->tb_nom->Location = System::Drawing::Point(553, 315);
+			   this->tb_nom->Location = System::Drawing::Point(553, 90);
 			   this->tb_nom->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			   this->tb_nom->Name = L"tb_nom";
 			   this->tb_nom->Size = System::Drawing::Size(316, 24);
@@ -188,7 +206,7 @@ namespace ProjetPOOGroupe2 {
 			   // 
 			   // tb_prenom
 			   // 
-			   this->tb_prenom->Location = System::Drawing::Point(553, 278);
+			   this->tb_prenom->Location = System::Drawing::Point(553, 38);
 			   this->tb_prenom->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			   this->tb_prenom->Name = L"tb_prenom";
 			   this->tb_prenom->Size = System::Drawing::Size(316, 24);
@@ -199,22 +217,23 @@ namespace ProjetPOOGroupe2 {
 			   this->label1->AutoSize = true;
 			   this->label1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
 				   static_cast<System::Int32>(static_cast<System::Byte>(255)));
-			   this->label1->Location = System::Drawing::Point(470, 318);
+			   this->label1->Location = System::Drawing::Point(622, 67);
 			   this->label1->Name = L"label1";
-			   this->label1->Size = System::Drawing::Size(76, 18);
+			   this->label1->Size = System::Drawing::Size(158, 18);
 			   this->label1->TabIndex = 8;
-			   this->label1->Text = L"Reference";
+			   this->label1->Text = L"Reference Commande";
+			   this->label1->Click += gcnew System::EventHandler(this, &MyForm3::label1_Click);
 			   // 
 			   // label2
 			   // 
 			   this->label2->AutoSize = true;
 			   this->label2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
 				   static_cast<System::Int32>(static_cast<System::Byte>(255)));
-			   this->label2->Location = System::Drawing::Point(428, 281);
+			   this->label2->Location = System::Drawing::Point(652, 12);
 			   this->label2->Name = L"label2";
-			   this->label2->Size = System::Drawing::Size(118, 18);
+			   this->label2->Size = System::Drawing::Size(104, 18);
 			   this->label2->TabIndex = 9;
-			   this->label2->Text = L"Date commande";
+			   this->label2->Text = L"ID Commande";
 			   // 
 			   // txt_results
 			   // 
@@ -245,7 +264,7 @@ namespace ProjetPOOGroupe2 {
 			   this->label3->AutoSize = true;
 			   this->label3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
 				   static_cast<System::Int32>(static_cast<System::Byte>(255)));
-			   this->label3->Location = System::Drawing::Point(381, 352);
+			   this->label3->Location = System::Drawing::Point(622, 119);
 			   this->label3->Name = L"label3";
 			   this->label3->Size = System::Drawing::Size(165, 18);
 			   this->label3->TabIndex = 15;
@@ -256,15 +275,15 @@ namespace ProjetPOOGroupe2 {
 			   this->label4->AutoSize = true;
 			   this->label4->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
 				   static_cast<System::Int32>(static_cast<System::Byte>(255)));
-			   this->label4->Location = System::Drawing::Point(481, 389);
+			   this->label4->Location = System::Drawing::Point(612, 171);
 			   this->label4->Name = L"label4";
-			   this->label4->Size = System::Drawing::Size(64, 18);
+			   this->label4->Size = System::Drawing::Size(187, 18);
 			   this->label4->TabIndex = 16;
-			   this->label4->Text = L"Contenu";
+			   this->label4->Text = L"Emission de la Commande";
 			   // 
 			   // textBox1
 			   // 
-			   this->textBox1->Location = System::Drawing::Point(553, 383);
+			   this->textBox1->Location = System::Drawing::Point(554, 194);
 			   this->textBox1->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			   this->textBox1->Name = L"textBox1";
 			   this->textBox1->Size = System::Drawing::Size(315, 24);
@@ -272,7 +291,7 @@ namespace ProjetPOOGroupe2 {
 			   // 
 			   // textBox2
 			   // 
-			   this->textBox2->Location = System::Drawing::Point(553, 349);
+			   this->textBox2->Location = System::Drawing::Point(553, 142);
 			   this->textBox2->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			   this->textBox2->Name = L"textBox2";
 			   this->textBox2->Size = System::Drawing::Size(316, 24);
@@ -290,12 +309,109 @@ namespace ProjetPOOGroupe2 {
 			   this->label_stock->TabIndex = 22;
 			   this->label_stock->Text = L"COMMANDES";
 			   // 
+			   // button1
+			   // 
+			   this->button1->BackColor = System::Drawing::SystemColors::InactiveCaption;
+			   this->button1->ForeColor = System::Drawing::SystemColors::HotTrack;
+			   this->button1->Location = System::Drawing::Point(232, 272);
+			   this->button1->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
+			   this->button1->Name = L"button1";
+			   this->button1->Size = System::Drawing::Size(211, 67);
+			   this->button1->TabIndex = 23;
+			   this->button1->Text = L"AJOUTER ARTICLE COMMANDE";
+			   this->button1->UseVisualStyleBackColor = false;
+			   // 
+			   // label5
+			   // 
+			   this->label5->AutoSize = true;
+			   this->label5->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				   static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			   this->label5->Location = System::Drawing::Point(638, 275);
+			   this->label5->Name = L"label5";
+			   this->label5->Size = System::Drawing::Size(125, 18);
+			   this->label5->TabIndex = 24;
+			   this->label5->Text = L"Date de Paiement";
+			   // 
+			   // label6
+			   // 
+			   this->label6->AutoSize = true;
+			   this->label6->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				   static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			   this->label6->Location = System::Drawing::Point(638, 223);
+			   this->label6->Name = L"label6";
+			   this->label6->Size = System::Drawing::Size(139, 18);
+			   this->label6->TabIndex = 25;
+			   this->label6->Text = L"Moyen de Paiement";
+			   // 
+			   // textBox3
+			   // 
+			   this->textBox3->Location = System::Drawing::Point(554, 298);
+			   this->textBox3->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
+			   this->textBox3->Name = L"textBox3";
+			   this->textBox3->Size = System::Drawing::Size(315, 24);
+			   this->textBox3->TabIndex = 26;
+			   // 
+			   // textBox4
+			   // 
+			   this->textBox4->Location = System::Drawing::Point(554, 246);
+			   this->textBox4->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
+			   this->textBox4->Name = L"textBox4";
+			   this->textBox4->Size = System::Drawing::Size(315, 24);
+			   this->textBox4->TabIndex = 27;
+			   // 
+			   // label7
+			   // 
+			   this->label7->AutoSize = true;
+			   this->label7->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				   static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			   this->label7->Location = System::Drawing::Point(605, 327);
+			   this->label7->Name = L"label7";
+			   this->label7->Size = System::Drawing::Size(206, 18);
+			   this->label7->TabIndex = 28;
+			   this->label7->Text = L"Nom de l\'article à commander";
+			   // 
+			   // textBox5
+			   // 
+			   this->textBox5->Location = System::Drawing::Point(553, 350);
+			   this->textBox5->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
+			   this->textBox5->Name = L"textBox5";
+			   this->textBox5->Size = System::Drawing::Size(315, 24);
+			   this->textBox5->TabIndex = 29;
+			   // 
+			   // label8
+			   // 
+			   this->label8->AutoSize = true;
+			   this->label8->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				   static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			   this->label8->Location = System::Drawing::Point(584, 379);
+			   this->label8->Name = L"label8";
+			   this->label8->Size = System::Drawing::Size(258, 18);
+			   this->label8->TabIndex = 30;
+			   this->label8->Text = L"Quantité de cette article à commander";
+			   // 
+			   // textBox6
+			   // 
+			   this->textBox6->Location = System::Drawing::Point(553, 402);
+			   this->textBox6->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
+			   this->textBox6->Name = L"textBox6";
+			   this->textBox6->Size = System::Drawing::Size(315, 24);
+			   this->textBox6->TabIndex = 31;
+			   // 
 			   // MyForm3
 			   // 
 			   this->AutoScaleDimensions = System::Drawing::SizeF(9, 18);
 			   this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			   this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			   this->ClientSize = System::Drawing::Size(882, 566);
+			   this->Controls->Add(this->textBox6);
+			   this->Controls->Add(this->label8);
+			   this->Controls->Add(this->textBox5);
+			   this->Controls->Add(this->label7);
+			   this->Controls->Add(this->textBox4);
+			   this->Controls->Add(this->textBox3);
+			   this->Controls->Add(this->label6);
+			   this->Controls->Add(this->label5);
+			   this->Controls->Add(this->button1);
 			   this->Controls->Add(this->label_stock);
 			   this->Controls->Add(this->textBox2);
 			   this->Controls->Add(this->textBox1);
@@ -418,5 +534,7 @@ namespace ProjetPOOGroupe2 {
 		}
 	}
 
-	};
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
